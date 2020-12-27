479a8be33d7b5738f4df5f19bb71fff124287f4d
ankbaid <ankbaid@gmail.com>
2020-12-28 00:22:21 +0530

    revisit the commitment

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World. Ready for 100 days of code challenge");
+        System.out.println("Revisit: Hello World. Ready for 100 days of code challenge");
     }
 }
