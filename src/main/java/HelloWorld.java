1ad73265004db145b771deff3c1a0e229fb4da37
ankbaid <ankbaid@gmail.com>
2022-01-05 16:45:42 +0530

    start of 2022

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World. Ready for 100 days of code challenge");
         System.out.println("Revisit: Hello World. Ready for 100 days of code challenge");
+        System.out.println("Revisit Revisit: Hello World. Ready for 100 days of code challenge : 2022");
     }
 }
